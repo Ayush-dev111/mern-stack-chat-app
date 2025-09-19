8b2f3aa3249e51344f1d9b4b53c8019e2a14b425
Ayush-dev111 <ayushnegi4d6f@gmail.com>
2025-09-19 17:35:28 +0530

    sidebar completed

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,122 +1,127 @@
 import { generateToken } from '../lib/utils.js';
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
-import {sendWelcomeEmail} from '../nodemailer/email.js';
+import { sendWelcomeEmail } from '../nodemailer/email.js';
 import { ENV } from '../lib/env.js';
 import cloudinary from '../lib/cloudinary.js';
 
 
-export const userSignup = async(req, res)=>{
-     const {fullName, email, password} = req.body;
+export const userSignup = async (req, res) => {
+    const { fullName, email, password } = req.body;
     try {
-       
 
-        if(!fullName || !email || !password){
-           return res.status(400).json({success: false, message: "All fields are required"});
+
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ success: false, message: "All fields are required" });
         };
 
-        if(password.length < 6){
-            return res.status(400).json({success: false, message: "password must be atleast 6 characters long"});
+        if (password.length < 6) {
+            return res.status(400).json({ success: false, message: "password must be at least 6 characters long" });
         }
 
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if(!regex.test(email)){
-            return res.status(400).json({success: false, message: "invalid email format"});
+        if (!regex.test(email)) {
+            return res.status(400).json({ success: false, message: "invalid email format" });
         }
+        
+        const user = await User.findOne({ email });
 
-        const user = await User.findOne({email});
-
-        if(user){
-            return res.status(400).json({success: false, message: "user already exists "});
+        if (user) {
+            return res.status(400).json({ success: false, message: "user already exists " });
         };
 
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        const newUser = new User ({
-            fullName : fullName,
-            email : email, 
+        const newUser = new User({
+            fullName: fullName,
+            email: email,
             password: hashedPassword
         });
 
         await newUser.save();
         generateToken(newUser._id, res);
-     
+
         res.status(201).json({
-            success: true,
-            message: "user signup successful",
-            data: {...newUser._doc, password: "undefined"}});
-        
-        sendWelcomeEmail(newUser.email, newUser.fullName , ENV.CLIENT_URL);
-        
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            email: newUser.email,
+            profilePic: newUser.profilePic,
+        })
+
+
+        sendWelcomeEmail(newUser.email, newUser.fullName, ENV.CLIENT_URL);
+
     } catch (error) {
         console.log("Error in signup route", error);
-        res.status(500).json({success: false, message: "error in signing up"});
+        res.status(500).json({ success: false, message: "error in signing up" });
     }
 };
 
-export const userLogin = async (req, res)=>{
-    const {email, password} = req.body;
+export const userLogin = async (req, res) => {
+    const { email, password } = req.body;
 
-    try {   
-        if(!email || !password){
-            return res.status(400).json({success: false, message: "all fields are required"});
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ success: false, message: "all fields are required" });
         };
 
-        const user = await User.findOne({email});
-        if(!user){
-            res.status(400).json({success: false, message: "Invalid credentials"});
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ success: false, message: "Invalid credentials" });
         }
 
         const isPassCorrect = await bcrypt.compare(password, user.password);
-        if(!isPassCorrect){
-            res.status(400).json({success: false, message: "Invalid credentials"});
+        if (!isPassCorrect) {
+            return res.status(400).json({ success: false, message: "Invalid credentials" });
         }
 
         generateToken(user._id, res);
 
         res.status(200).json({
-            success: true,
-            message: "Login successful"
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
         });
-
     } catch (error) {
         console.log("Error in login route :", error);
-        res.status(500).json({success: false,  message: "internal server error"});
+        res.status(500).json({ success: false, message: "internal server error" });
     };
 };
 
-export const userLogout = (_, res)=>{
-    res.cookie("jwt", " ", {maxAge: 0});
+export const userLogout = (_, res) => {
+    res.cookie("jwt", " ", { maxAge: 0 });
     res.status(200).json({
         success: true,
         message: "Successfully logged out"
     })
 };
 
-export  const updateProfile = async (req, res) =>{
-    try{
-        const {profilePic} = req.body;
-        if(!profilePic){
-            return res.status(400).json({success: false, message: "Profile pic is required"});
+export const updateProfile = async (req, res) => {
+    try {
+        const { profilePic } = req.body;
+
+        if (!profilePic) {
+            return res.status(400).json({ success: false, message: "Profile pic is required" });
         }
-        
-        const userId = req.user_id;
+
+        const userId = req.user._id;
         const uploadResource = await cloudinary.uploader.upload(profilePic);
 
-        const updatedUser = User.findByIdAndUpdate(
+        const updatedUser = await User.findByIdAndUpdate(
             userId,
-            {profilePic: uploadResource.secure_url},
-            {new: true}
+            { profilePic: uploadResource.secure_url },
+            { new: true }
         )
 
-        res.status(200).json({success: true, data: updatedUser});
-    }catch(error){
+        res.status(200).json(updatedUser);
+    } catch (error) {
         console.log("Error in update Profile controller :", error);
-        res.status(500).json({success: false, message: "Internal server error"});
+        res.status(500).json({ success: false, message: "Internal server error" });
     };
 };
 
-export const  checkAuth = (req, res) =>{
-    res.status(200).json({success: true, data: req.user})
+export const checkAuth = (req, res) => {
+    res.status(200).json(req.user )
 };
--- frontend/src/components/ActiveTabSwitch.jsx
+++ frontend/src/components/ActiveTabSwitch.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useChatStore } from "../store/useChatStore";
 
-const ActiveTabSwitch = () => {
+function ActiveTabSwitch() {
+  const { activeTab, setActiveTab } = useChatStore();
+
   return (
-    <div>ActiveTabSwitch</div>
-  )
-}
+    <div role="tablist" className="tabs tabs-boxed bg-transparent p-2 m-2 mx-auto">
+      <button
+        onClick={() => setActiveTab("chats")}
+        role="tab"
+        className={`tab w-35 ${
+          activeTab === "chats" ? "bg-cyan-500/20 text-cyan-600" : "text-slate-400"
+        }`}
+      >
+        Chats
+      </button>
 
-export default ActiveTabSwitch
+      <button
+        onClick={() => setActiveTab("contacts")}
+        role="tab"
+        className={`tab w-35 ${
+          activeTab === "contacts" ? "bg-cyan-500/20 text-cyan-600" : "text-slate-400"
+        }`}
+      >
+        Contacts
+      </button>
+    </div>
+  );
+}
+export default ActiveTabSwitch;
