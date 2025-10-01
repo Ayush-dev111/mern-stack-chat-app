175333bed94d0a5fae3350d50a625aab3ceee34b
Ayush-dev111 <ayushnegi4d6f@gmail.com>
2025-10-01 16:53:29 +0530

    deploying  app

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3500/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3500/api" : "https://mern-stack-chat-app-x4ny.onrender.com",
   withCredentials: true,
 });
